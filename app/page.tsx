b3c73086b2407950b967b19590b0fe3120d6cb4d
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-19 22:59:03 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import { cookies } from "next/headers";
+
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const token = cookies().get("token");
-
-  if (token) {
-    redirect("/boards");
-  } else {
-    redirect("/login");
-  }
+  redirect("/login");
 }
